eeb621a8b7209b30b2f8ba9931431778cda9a003
calculatorai <unicode2023@yeah.net>
2023-12-10 21:42:50 +0800

    #add tag

--- code/app/layout.tsx
+++ code/app/layout.tsx
@@ -15,8 +15,8 @@ import StyledComponentsRegistry from '@/lib/AntdRegistry';
 
 
 export default function RootLayout({
-                                     children
-                                   }: {
+  children
+}: {
   children: React.ReactNode;
 }) {
   const pathname = usePathname();
@@ -24,22 +24,30 @@ export default function RootLayout({
 
   return (
     <html suppressHydrationWarning lang={locale(pathname)}>
-    <head>
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-      <link rel="manifest" href="/site.webmanifest"></link>
-      <meta name="privacy-policy" content="/privacy.html" />
-    </head>
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest"></link>
+        <meta name="privacy-policy" content="/privacy.html" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-XH4PKFQ459"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
 
-    <body className={`bg-[#FCFCFC] dark:bg-black ${inter.className}`}>
-    <Providers>
-      <Header />
-      <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
-      <Footer />
-      <ScrollToTop />
-    </Providers>
-    </body>
+          gtag('config', 'G-XH4PKFQ459');
+        </script>
+      </head>
+
+      <body className={`bg-[#FCFCFC] dark:bg-black ${inter.className}`}>
+        <Providers>
+          <Header />
+          <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+          <Footer />
+          <ScrollToTop />
+        </Providers>
+      </body>
     </html>
   );
 }
